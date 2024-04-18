21e0c373fa67566aeac76e86e48c9e7ac6219d12
Kalleby Evangelho <82461649+KallebyX@users.noreply.github.com>
2024-04-18 10:18:34 -0300

    Refactor MLModel to support flexible model types

--- src/machine_learning/model.py
+++ src/machine_learning/model.py
@@ -3,9 +3,15 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 
 class MLModel:
-    def __init__(self):
-        self.model = RandomForestClassifier(n_estimators=100)
+    def __init__(self, model_type='RandomForest', n_estimators=100):
+        self.model = self.load_model(model_type, n_estimators)
         self.scaler = StandardScaler()
+
+    def load_model(self, model_type, n_estimators):
+        if model_type == 'RandomForest':
+            return RandomForestClassifier(n_estimators=n_estimators)
+        # Add support for more models here
+        raise ValueError("Model type not supported")
 
     def train(self, X_train, y_train):
         X_train_scaled = self.scaler.fit_transform(X_train)
@@ -14,3 +20,4 @@ class MLModel:
     def predict(self, X):
         X_scaled = self.scaler.transform(X)
         return self.model.predict(X_scaled)
+
